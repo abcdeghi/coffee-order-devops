994263ebc7975c6f2c2297aa61ae03894e0f9902
abcdeghi <semin4599@naver.com>
2026-09-12 20:55:04 +0900

    fix: allow deployed frontend origin

--- back/src/main/java/com/back/nbe12141team07/global/jpa/entity/webMvc/WebMvcConfig.java
+++ back/src/main/java/com/back/nbe12141team07/global/jpa/entity/webMvc/WebMvcConfig.java
@@ -10,9 +10,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://cdpn.io", "http://localhost:3000", "http://127.0.0.1:3000")
+                .allowedOrigins("https://cdpn.io", "http://localhost:3000", "http://127.0.0.1:3000", "https://coffee-order-front.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
-}
+}
